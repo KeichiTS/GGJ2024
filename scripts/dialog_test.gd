b6f14ae6b64f4ee2a39d85dedc6cd2073471ab5e
KeichiTS <kkeichi@gmail.com>
2024-01-25 00:36:09 -0300

    dialog_system

---
+++ scripts/dialog_test.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+const lines: Array[String] = [
+	'test test test test test test test test test ',
+	'asdasdasdasdasdasdasdasdasd',
+	'qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq',
+	'eeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+	'ffffffffffffffffffffffffffffff'
+]
+
+
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		DialogManager.start_dialog(Vector2(300,300), lines)
